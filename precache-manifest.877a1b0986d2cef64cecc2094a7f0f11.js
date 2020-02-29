21e47c1117f5736ee3996fa217474329f27b4791
Akadar.akku <akadar.akku121@gmail.com>
2020-02-29 20:00:46 +0100

    Updates

---
+++ precache-manifest.877a1b0986d2cef64cecc2094a7f0f11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c8297874f0e8381fa97ef40cc8a714",
+    "url": "/exercise-app/index.html"
+  },
+  {
+    "revision": "33b76091086a80f980dc",
+    "url": "/exercise-app/static/js/2.0f598f27.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/exercise-app/static/js/2.0f598f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e51aa04ed36d56230731",
+    "url": "/exercise-app/static/js/main.f815e9e4.chunk.js"
+  },
+  {
+    "revision": "adfb1a2c9fdaf8f49b05",
+    "url": "/exercise-app/static/js/runtime-main.2d4b7aeb.js"
+  }
+]);
